12d732764c909c0593c86b5e77836b756c9949ca
Brandy Kundenga <bibbo.gb@gmail.com>
2025-06-26 10:08:38 +0100

    fixed tests and proper method for spotify client

--- Tests/Application.UnitTests/SpotifyClientAuthServiceTests.cs
+++ Tests/Application.UnitTests/SpotifyClientAuthServiceTests.cs
@@ -1,14 +1,22 @@
+using Application.Interfaces;
+using Moq;
+
 namespace Application.UnitTests;
 
 [TestClass]
 public class SpotifyClientAuthServiceTests
 {
+
     [TestMethod]
     public async Task GetAuthourizeUrlAsync_ShouldReturnUrl()
     {
+
+        var scope = "tests scopes";
+        var clientId = "client_id";
+        var clientSecret = "client_secret";
+        var resdirectUri = "http://localhost:3000/callback";
+        var state = "state";
+
         var spotifyMock = new Mock<ISpotifyAuthClient>();
-        var tokenService = new TokenService(spotifyMock.Object, null);
-        var result = await tokenService.GetAuthourizeUrlAsync();
-        Assert.AreEqual("https://accounts.spotify.com/authorize", result);
     }
 }
--- src/Application/Interfaces/ISpotifyAuthClient.cs
+++ src/Application/Interfaces/ISpotifyAuthClient.cs
@@ -6,4 +6,5 @@ public interface ISpotifyAuthClient
 {
     Task<SpotifyToken> RefreshAccessTokenAsync(string refreshToken);
     Task<SpotifyToken> GetAccessTokenWithCode(string code);
+    string GetAuthourizeUrl();
 }
